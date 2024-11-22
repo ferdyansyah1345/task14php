5977ce1725502be9864dab8a5a9c8390dd6f4744
ferdyansyah1345 <ferdysambo1345@gmail.com>
2024-11-22 14:20:20 +0700

    Add files via upload

---
+++ detail_Pelanggan.php
@@ -0,0 +1,10 @@
+<?php 
+    include_once 'header.php';
+?>
+<?php 
+    include_once 'sidebar.php';
+    include_once 'models/koneksi.php';
+    include_once 'd_pelanggan.php';
+?>
+<?php 
+    include_once 'footer.p
